2d80ddaa5f0fbf6fb85bb20842855087980a3fb1
Vasily Tarasov <tarasov@vasily.name>
2012-08-07 15:41:42 -0700

    Print the line number where an error occured in parenthesis

--- misc.c
+++ misc.c
@@ -331,7 +331,7 @@ fatal:
 		    line);
 
 		if (my_procflow == NULL)
-			(void) fprintf(stderr, " on line %d", lex_lineno);
+			(void) fprintf(stderr, " (line %d)", lex_lineno);
 
 		(void) fprintf(stderr, "\n");
 		(void) fflush(stderr);
